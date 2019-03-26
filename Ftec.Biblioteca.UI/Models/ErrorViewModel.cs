fa903b3510b67170c24a560bdc225a11bdbd4b5c
CesarT33 <cesar.lemos@dbserver.com.br>
2019-03-26 19:28:55 -0300

    feat: initial commit

---
+++ Ftec.Biblioteca.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ftec.Biblioteca.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
